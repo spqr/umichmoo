102e1a37d585393f3b55188721d989542d01630b
Ben Ransford <ransford@cs.umass.edu>
2011-06-14 15:49:19 -0400

    license is properly in license.txt

--- moo.c
+++ moo.c
@@ -1,40 +1,10 @@
-/*
-Copyright (c) 2009, Intel Corporation
-Copyright (c) 2011, University of Massachusetts Amherst
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-
- 1. Redistributions of source code must retain the above copyright
-    notice, this list of conditions and the following disclaimer.
- 2. Redistributions in binary form must reproduce the above copyright
-    notice, this list of conditions and the following disclaimer in the
-    documentation and/or other materials provided with the distribution.
- 3. Neither the name of the University of Massachusetts Amherst, nor the
-    name of Intel Corporation, nor the names of contributors may be used
-    to endorse or promote products derived from this software without
-    specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+/* See license.txt for license information. */
 
 //******************************************************************************
-//  Moo 1.0
+//  Moo 1.1
 //  Device: MSP430-2618
 //
-//  Version HW1.0_SW1.0
+//  Version HW1.1_SW1.1
 //
 //  Pinout:
 //             P1.1 = Data out signal
@@ -48,7 +18,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //	       Alien reader code is out of date
 //
 //	This is a partial implementation of the RFID EPCGlobal C1G2 protocol
-//	for the Moo 1.0 hardware platform.
+//	for the Moo 1.1 hardware platform.
 //	
 //	What's missing: 
 //        - SECURED and KILLED states.
