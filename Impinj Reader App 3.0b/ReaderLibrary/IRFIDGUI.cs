a5da18240da89ae6c985495f3e26f108afb37730
Ben Ransford <ransford@cs.umass.edu>
2011-06-20 16:22:02 -0400

    removed Impinj Reader App; use http://github.com/spqr/impinj-reader-app instead

--- Impinj Reader App 3.0b/ReaderLibrary/IRFIDGUI.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace ReaderLibrary
-{
-    public interface IRFIDGUI
-    {
-        void AppendToDebugTextBox(string appendToTop);
-        void AppendToMainTextBox(string appendToTop);
-    }
-}
