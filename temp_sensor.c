7b13be90e3610ddba3ce21c18b64aeb866c96f3a
Michael Rushanan <micharu123@gmail.com>
2013-06-12 16:01:00 -0400

    temp_sensor modeled after quick_accel_sensor. Using ACTIVE_SENSOR == SENSOR_EXTERNAL_TEMP to define temp sensor use. Currently have a linker erorr due to redefined function name.

--- temp_sensor.c
+++ temp_sensor.c
@@ -4,28 +4,40 @@
 #include "rfid.h"
 #include "temp_sensor.h"
 
-unsigned int read_temperature(void) {
-    unsigned int i, adcval;
+unsigned char sensor_busy = 0;
 
-    // setup ADC to read external analog temperature sensor
-    ADC12CTL0 &= ~ENC;                              // make sure this is off otherwise settings are locked.
-    P6SEL |= TEMP_EXT_IN;                           // Enable A/D channel A4
-    ADC12CTL0 = ADC12ON + SHT0_2 + REFON + REF2_5V; // Turn on and set up ADC12
-    ADC12CTL1 = SHP;                                // Use sampling timer
-    ADC12MCTL0 = INCH_TEMP_EXT_IN + SREF_1;         // Vr+=Vref+
+void init_sensor() {
+  return;
+}
 
-    // turn on temperature sensor and allow it at least 0.7-0.8 ms to stabilize
-    P1DIR |= TEMP_POWER;
-    P1OUT |= TEMP_POWER;
-    for (i = 0; i != 100; ++i);
+void read_sensor(unsigned char volatile *target) {
+  unsigned int i;
 
-    ADC12CTL0 |= ENC | ADC12SC;    // enable and start conversion
-    _BIC_SR(GIE);                  // disable interrupts while busy-waiting on ADC
-    while (ADC12CTL1 & ADC12BUSY); // busy-wait for ADC to sample
-    adcval = ADC12MEM0;
-    ADC12CTL0 = ADC12CTL1 = 0;     // turn off ADC
-    P1OUT &= ~TEMP_POWER;          // turn off temperature sensor
-    _BIS_SR(GIE);                  // reenable interrupts
+  // setup ADC to read external analog temperature sensor
+  ADC12CTL0 &= ~ENC;                              // make sure this is off otherwise settings are locked.
+  P6SEL |= TEMP_EXT_IN;                           // Enable A/D channel A4
+  ADC12CTL0 = ADC12ON + SHT0_2 + REFON + REF2_5V; // Turn on and set up ADC12
+  ADC12CTL1 = SHP;                                // Use sampling timer
+  ADC12MCTL0 = INCH_TEMP_EXT_IN + SREF_1;         // Vr+=Vref+
 
-    return adcval;
+  // turn on temperature sensor and allow it at least 0.7-0.8 ms to stabilize
+  P1DIR |= TEMP_POWER;
+  P1OUT |= TEMP_POWER;
+  for (i = 0; i != 100; ++i);
+
+  ADC12CTL0 |= ENC | ADC12SC;    // enable and start conversion
+  _BIC_SR(GIE);                  // disable interrupts while busy-waiting on ADC
+  while (ADC12CTL1 & ADC12BUSY); // busy-wait for ADC to sample 
+  ackReply[4] = (ADC12MEM0 & 0xff);        // grab that data.
+  ackReply[3] = (ADC12MEM0 & 0x0f00) >> 8; // grab msb bits and store it
+  ADC12CTL0 = ADC12CTL1 = 0;               // turn off ADC
+  P1OUT &= ~TEMP_POWER;          // turn off temperature sensor
+  
+  // Reenable interrupts.
+  _BIS_SR(GIE);
+  
+  // Store sensor read count. I assume this should be after reenabling interrupts.
+  sensor_counter++;                              
+  ackReply[10] = (sensor_counter & 0x00ff);      
+  ackReply[9]  = (sensor_counter & 0xff00) >> 8; // grab msb bits and store it
 }
--- temp_sensor.h
+++ temp_sensor.h
@@ -1,3 +1,11 @@
 /* See license.txt for license information. */
+#define SENSOR_DATA_TYPE_ID       0x1B
 
-unsigned int read_temperature(void);
+#define DATA_LENGTH_IN_WORDS      1
+#define DATA_LENGTH_IN_BYTES      (DATA_LENGTH_IN_WORDS*2)
+
+extern unsigned char sensor_busy;
+
+void init_sensor();
+
+void read_sensor(unsigned char volatile *);
