4b3fc46844fff9592dabd0fd072e1a571a7dd264
Mark Schulte <schultetwin@gmail.com>
2014-09-01 15:03:07 -0400

    [gnu] Adding __delay_cycles() and MESSAGE()

--- compiler.h
+++ compiler.h
@@ -1,6 +1,10 @@
 #ifndef __MOO_COMPILIERS_H__
 #define __MOO_COMPILIERS_H__
 
+/* Helper functions */
+#define QUOTE(str) #str
+#define EXPAND_AND_QUOTE(str) QUOTE(str)
+
 #ifdef __GNUC__
 /* GNU C compiler supported */
 #elif __IAR_SYSTEMS_ICC__
@@ -19,13 +23,33 @@
 
 /* Lock Register */
 #ifdef __GNUC__
-	#define __LOCK_REG_QUOTE(str) #str
-	#define __LOCK_REG_EXPAND_AND_QUOTE(str) __LOCK_REG_QUOTE(str)
-	#define LOCK_REG(reg_num, var_decl) register var_decl asm(__LOCK_REG_EXPAND_AND_QUOTE(R ## reg_num))
+	#define LOCK_REG(reg_num, var_decl) register var_decl asm(EXPAND_AND_QUOTE(R ## reg_num))
 #elif __IAR_SYSTEMS_ICC__
 	#define LOCK_REG(reg_num, var_decl) __regvar __no_init var_decl @ ## num ##
 #endif
 /* End Lock Register */
 
+/* Delay cycles */
+#ifdef __GNUC__
+	static inline void __delay_cycles(unsigned int n) {
+		__asm__ __volatile__ (
+				"1: \n"
+				" dec %[n] \n"
+				" jne 1b \n"
+				: [n] "+r"(n));
+	}
+#elif __IAR_SYSTEMS_ICC__
+	/* Defined by the compilier */
+#endif
+/* End Delay cycles */
+
+/* Pragma Message */
+#ifdef __GNUC__
+	#define DO_PRAGMA(x) _Pragma (#x)
+	#define MESSAGE(msg) DO_PRAGMA(message (msg))
+#elif __IAR_SYSTEMS_ICC__
+	#define MESSAGE(msg) #pragma message (msg)
+#endif
+/* End Pragma Message */
 
 #endif /*__MOO_COMPILIERS_H__ */
