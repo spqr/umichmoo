2ba6fec44d3fe42937fbce30dc11ce3f94c9dceb
Mark Schulte <schultetwin@gmail.com>
2014-09-01 15:03:51 -0400

    [gnu] Adding new MESSAGE directive

--- sensor.h
+++ sensor.h
@@ -17,22 +17,22 @@ void init_sensors();
 #if SIMPLE_QUERY_ACK
 #define ENABLE_READS                  0
 #define READ_SENSOR                   0
-#pragma message ("compiling simple query-ack application")
+MESSAGE("compiling simple query-ack application")
 #endif
 #if SENSOR_DATA_IN_ID
 #define ENABLE_READS                  0
 #define READ_SENSOR                   1
-#pragma message ("compiling sensor data in id application")
+MESSAGE("compiling sensor data in id application")
 #endif
 #if SIMPLE_READ_COMMAND
 #define ENABLE_READS                  1
 #define READ_SENSOR                   0
-#pragma message ("compiling simple read command application")
+MESSAGE("compiling simple read command application")
 #endif
 #if SENSOR_DATA_IN_READ_COMMAND
 #define ENABLE_READS                  1
 #define READ_SENSOR                   1
-#pragma message ("compiling sensor data in read command application")
+MESSAGE("compiling sensor data in read command application")
 #endif
 
 #endif //SENSOR_CONF_H
